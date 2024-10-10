21f56f0c37b1c019131e12e846be2fa548186930
UT01717 <UT01717@UNICOMTIC4>
2024-10-10 12:54:50 +0530

    Add project files.

---
+++ Bike.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motorbikemanagement
+{
+   public class Bike
+    {
+        public int BikeId { get; set; }
+        public string Brand {  get; set; }
+        public string model { get; set; }
+        public decimal RentalPrice {  get; set; }
+
+    }
+}
